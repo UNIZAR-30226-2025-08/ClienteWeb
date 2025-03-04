896354dcf4303bd8a9af8b1756ea2d81e2244e5e
AdrianBecerril-uni <873026@unizar.es>
2025-03-05 00:13:03 +0100

    cambios

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,4 @@ export default {}
 </script>
 
 <style>
-/* Estilos globales (no usar scoped) */
-html, body {
-  margin: 0;
-  padding: 0;
-  background-color: #302E2B !important;
-  /* Fuerza a que no haya imagen de fondo */
-  background-image: none !important;
-  background-repeat: no-repeat !important;
-  background-size: auto !important;
-}
 </style>
---
+++ src/components/Toastejemplos.vue
@@ -0,0 +1,37 @@
+<template>
+    <!-- Aquí no es necesario agregar nada visible en el template -->
+  </template>
+  
+  <script setup>
+  import { toast } from 'vue3-toastify';
+  import 'vue3-toastify/dist/index.css';
+  
+  // Función para mostrar una notificación
+  function showToast(message, type = 'info') {
+    switch(type) {
+      case 'success':
+        toast.success(message, { autoClose: 2000 });
+        break;
+      case 'error':
+        toast.error(message, { autoClose: 3000 });
+        break;
+      case 'warning':
+        toast.warning(message, { autoClose: 2000 });
+        break;
+      case 'info':
+      default:
+        toast.info(message, { autoClose: 2000 });
+        break;
+    }
+  }
+  
+  // Puedes exponer la función showToast para que sea accesible globalmente
+  defineExpose({
+    showToast,
+  });
+  </script>
+  
+  <style scoped>
+  /* Estilos adicionales (si los necesitas) */
+  </style>
+  
