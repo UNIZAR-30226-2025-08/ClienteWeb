db9a6aa5125d9d67c0c82c5c815569587091733a
Oscar-GilBe <870919@unizar.es>
2025-05-01 20:59:49 +0200

    uso de SSL

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 // src/utils/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://albertofsg.ddns.net:5000");
+const socket = io("https://albertofsg.ddns.net:5000");
 
 export default socket;
