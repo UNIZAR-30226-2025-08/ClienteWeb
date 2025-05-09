8d7ad41e519cf3d74f6307c13e3ed5a89dc457d0
Nicofaroo <871820@unizar.es>
2025-05-09 18:24:11 +0200

    reversion de cambios

--- src/views/partida/partida.vue
+++ src/views/partida/partida.vue
@@ -594,8 +594,6 @@ export default {
     socket.on("iniciarVotacionAlguacil", (data) => {
       console.log("Votación del alguacil iniciada", data);
       this.flujoVotacionAlguacil();
-      this.changePhase("intro");
-      this.changePhase("fin_turno_lobos");
       this.timeLeft = 24; // Tiempo que nos envia el backend sino pongo el tiempo que de momento se ha estimado en backend (revisar partida.js o partidaws)
       this.isVotingPhase = true;
     });
